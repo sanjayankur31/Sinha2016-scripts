5aa51f7d6e76a6e1d6906ce784000a0c16e75d05
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-02-09 18:08:54 +0000

    Process log files for multiple patterns

--- postprocess/bash/nest/consolidate-spikes.sh
+++ postprocess/bash/nest/consolidate-spikes.sh
@@ -20,25 +20,42 @@
 #
 
 SORTTMPDIR="/home/asinha/dump/sort-tmpdir"
+# Get number of patterns in this simulation
+NUMPATS="$(ls spikes-pattern* | grep -Eo 'pattern-[0-9]+' | sort  | uniq | sed 's/pattern-//' | wc -l)"
 
 echo "Combining files for NEST simulation"
 mkdir consolidated_files
 
-echo "Combining recall files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*recall*.gdf > spikes-recall.gdf
-mv spikes-recall.gdf consolidated_files
+# Merge multiple pattern files
+for pat in $(seq 1 $NUMPATS); do
+    echo "Combining background files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-background-""$pat"*.gdf  > "spikes-background-""$pat"".gdf"
+    mv "spikes-background-""$pat"".gdf" consolidated_files
 
-echo "Combining pattern files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*pattern*.gdf > spikes-pattern.gdf
-mv spikes-pattern.gdf consolidated_files
+    echo "Combining deaffed-bg-E files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-deaffed-bg-E-""$pat"*.gdf  > "spikes-deaffed-bg-E-""$pat"".gdf"
+    mv "spikes-deaffed-bg-E-""$pat"".gdf" consolidated_files
 
-echo "Combining deaff files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*deaffed*.gdf > spikes-deaffed-pattern.gdf
-mv spikes-deaffed-pattern.gdf consolidated_files
+    echo "Combining deaffed-bg-I files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-deaffed-bg-I-""$pat"*.gdf  > "spikes-deaffed-bg-I-""$pat"".gdf"
+    mv "spikes-deaffed-bg-I-""$pat"".gdf" consolidated_files
 
-echo "Combining background files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*background*.gdf > spikes-background.gdf
-mv spikes-background.gdf consolidated_files
+    echo "Combining deaffed-pattern files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-deaffed-pattern-""$pat"*.gdf  > "spikes-deaffed-pattern-""$pat"".gdf"
+    mv "spikes-deaffed-pattern-""$pat"".gdf" consolidated_files
+
+    echo "Combining pattern files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-pattern-""$pat"*.gdf  > "spikes-pattern-""$pat"".gdf"
+    mv "spikes-pattern-""$pat"".gdf" consolidated_files
+
+    echo "Combining recall files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-recall-""$pat"*.gdf  > "spikes-recall-""$pat"".gdf"
+    mv "spikes-recall-""$pat"".gdf" consolidated_files
+
+    echo "Combining stim files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-stim-""$pat"*.gdf  > "spikes-stim-""$pat"".gdf"
+    mv "spikes-stim-""$pat"".gdf" consolidated_files
+done
 
 echo "Combining E files"
 LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*E*.gdf > spikes-E.gdf
@@ -47,10 +64,6 @@ mv spikes-E.gdf consolidated_files
 echo "Combining I files"
 LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*I*.gdf > spikes-I.gdf
 mv spikes-I.gdf consolidated_files
-
-echo "Combining stim files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*stim*.gdf > spikes-stim.gdf
-mv spikes-stim.gdf consolidated_files
 
 echo "All files combined."
 exit 0
