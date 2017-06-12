8d46ac17a8d8a346ca365e8a98339daeb71fc996
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-06-12 17:25:46 +0100

    Update spike files that need to be consolidated

--- postprocess/bash/nest/consolidate-files.sh
+++ postprocess/bash/nest/consolidate-files.sh
@@ -30,29 +30,29 @@ mkdir consolidated_files
 
 # Merge multiple pattern files
 for pat in $(seq 1 $NUMPATS); do
-    echo "Combining background spike files"
-    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-background-""$pat"*.gdf  > "spikes-background-""$pat"".gdf"
-    mv "spikes-background-""$pat"".gdf" consolidated_files
-
-    echo "Combining deaffed-bg-E spike files"
-    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-deaffed-bg-E-""$pat"*.gdf  > "spikes-deaffed-bg-E-""$pat"".gdf"
-    mv "spikes-deaffed-bg-E-""$pat"".gdf" consolidated_files
-
-    echo "Combining deaffed-bg-I spike files"
-    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-deaffed-bg-I-""$pat"*.gdf  > "spikes-deaffed-bg-I-""$pat"".gdf"
-    mv "spikes-deaffed-bg-I-""$pat"".gdf" consolidated_files
-
-    echo "Combining deaffed-pattern spike files"
-    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-deaffed-pattern-""$pat"*.gdf  > "spikes-deaffed-pattern-""$pat"".gdf"
-    mv "spikes-deaffed-pattern-""$pat"".gdf" consolidated_files
-
     echo "Combining pattern spike files"
     LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-pattern-""$pat"*.gdf  > "spikes-pattern-""$pat"".gdf"
     mv "spikes-pattern-""$pat"".gdf" consolidated_files
 
+    echo "Combining background spike files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-background-""$pat"*.gdf  > "spikes-background-""$pat"".gdf"
+    mv "spikes-background-""$pat"".gdf" consolidated_files
+
     echo "Combining recall spike files"
     LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-recall-""$pat"*.gdf  > "spikes-recall-""$pat"".gdf"
     mv "spikes-recall-""$pat"".gdf" consolidated_files
+
+    echo "Combining lpz-pattern spike files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-lpz-pattern-""$pat"*.gdf  > "spikes-lpz-pattern-""$pat"".gdf"
+    mv "spikes-lpz-pattern-""$pat"".gdf" consolidated_files
+
+    echo "Combining lpz-bg-E spike files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-lpz-bg-E-""$pat"*.gdf  > "spikes-lpz-bg-E-""$pat"".gdf"
+    mv "spikes-lpz-bg-E-""$pat"".gdf" consolidated_files
+
+    echo "Combining lpz-bg-I spike files"
+    LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-lpz-bg-I-""$pat"*.gdf  > "spikes-lpz-bg-I-""$pat"".gdf"
+    mv "spikes-lpz-bg-I-""$pat"".gdf" consolidated_files
 
     echo "Combining stim spike files"
     LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR "spikes-stim-""$pat"*.gdf  > "spikes-stim-""$pat"".gdf"
@@ -62,13 +62,13 @@ for pat in $(seq 1 $NUMPATS); do
     LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "patternneurons-""$pat""-rank-"*.txt | uniq > "patternneurons-""$pat"".txt"
     mv "patternneurons-""$pat"".txt" consolidated_files
 
-    echo "Combining deaffed pattern neuron files"
-    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "deaffed-patternneurons-""$pat""-rank-"*.txt | uniq > "deaffed-patternneurons-""$pat"".txt"
-    mv "deaffed-patternneurons-""$pat"".txt" consolidated_files
+    echo "Combining lpz pattern neuron files"
+    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "lpz-patternneurons-""$pat""-rank-"*.txt | uniq > "lpz-patternneurons-""$pat"".txt"
+    mv "lpz-patternneurons-""$pat"".txt" consolidated_files
 
-    echo "Combining non deaffed pattern neuron files"
-    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "non-deaffed-patternneurons-""$pat""-rank-"*.txt | uniq > "non-deaffed-patternneurons-""$pat"".txt"
-    mv "non-deaffed-patternneurons-""$pat"".txt" consolidated_files
+    echo "Combining non lpz pattern neuron files"
+    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "non-lpz-patternneurons-""$pat""-rank-"*.txt | uniq > "non-lpz-patternneurons-""$pat"".txt"
+    mv "non-lpz-patternneurons-""$pat"".txt" consolidated_files
 
     echo "Combining recall neuron files"
     LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "recallneurons-""$pat""-rank-"*.txt | uniq > "recallneurons-""$pat"".txt"
@@ -78,30 +78,38 @@ for pat in $(seq 1 $NUMPATS); do
     LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "backgroundneurons-""$pat""-rank-"*.txt | uniq > "backgroundneurons-""$pat"".txt"
     mv "backgroundneurons-""$pat"".txt" consolidated_files
 
-    echo "Combining deaffed background neuron files"
-    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "deaffed-backgroundneurons-""$pat""-rank-"*.txt | uniq > "deaffed-backgroundneurons-""$pat"".txt"
-    mv "deaffed-backgroundneurons-""$pat"".txt" consolidated_files
+    echo "Combining lpz background neuron files"
+    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "lpz-backgroundneurons-""$pat""-rank-"*.txt | uniq > "lpz-backgroundneurons-""$pat"".txt"
+    mv "lpz-backgroundneurons-""$pat"".txt" consolidated_files
 
-    echo "Combining non deaffed background neuron files"
-    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "non-deaffed-backgroundneurons-""$pat""-rank-"*.txt | uniq > "non-deaffed-backgroundneurons-""$pat"".txt"
-    mv "non-deaffed-backgroundneurons-""$pat"".txt" consolidated_files
+    echo "Combining non lpz background neuron files"
+    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "non-lpz-backgroundneurons-""$pat""-rank-"*.txt | uniq > "non-lpz-backgroundneurons-""$pat"".txt"
+    mv "non-lpz-backgroundneurons-""$pat"".txt" consolidated_files
 
-    echo "Combining deaffed I neuron files"
-    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "deaffed-Ineurons-""$pat""-rank-"*.txt | uniq > "deaffed-Ineurons-""$pat"".txt"
-    mv "deaffed-Ineurons-""$pat"".txt" consolidated_files
+    echo "Combining lpz I neuron files"
+    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "lpz-Ineurons-""$pat""-rank-"*.txt | uniq > "lpz-Ineurons-""$pat"".txt"
+    mv "lpz-Ineurons-""$pat"".txt" consolidated_files
 
-    echo "Combining non deaffed I neuron files"
-    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "non-deaffed-Ineurons-""$pat""-rank-"*.txt | uniq > "non-deaffed-Ineurons-""$pat"".txt"
-    mv "non-deaffed-Ineurons-""$pat"".txt" consolidated_files
+    echo "Combining non lpz I neuron files"
+    LC_ALL=C sort -n --parallel=16 -T $SORTTMPDIR "non-lpz-Ineurons-""$pat""-rank-"*.txt | uniq > "non-lpz-Ineurons-""$pat"".txt"
+    mv "non-lpz-Ineurons-""$pat"".txt" consolidated_files
 done
 
 echo "Combining E files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*E*.gdf > spikes-E.gdf
+LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-E*.gdf > spikes-E.gdf
 mv spikes-E.gdf consolidated_files
 
 echo "Combining I files"
-LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-*I*.gdf > spikes-I.gdf
+LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-I*.gdf > spikes-I.gdf
 mv spikes-I.gdf consolidated_files
+
+echo "Combining LPZ E files"
+LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-lpz-E*.gdf > spikes-lpz-E.gdf
+mv spikes-lpz-E.gdf consolidated_files
+
+echo "Combining LPZ I files"
+LC_ALL=C sort -k "2" -n --parallel=16 -T $SORTTMPDIR spikes-lpz-I*.gdf > spikes-lpz-I.gdf
+mv spikes-lpz-I.gdf consolidated_files
 
 echo "Removed $SORTTMPDIR"
 rm -rf $SORTTMPDIR
