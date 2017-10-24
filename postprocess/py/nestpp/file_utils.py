1c220cb20b235848336c9644541a67c148250c02
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-10-24 17:45:12 +0100

    Use globbing
    [skip ci]

--- postprocess/py/nestpp/file_utils.py
+++ postprocess/py/nestpp/file_utils.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 """
-Enter one line description here.
+Utilities dealing with files.
 
-File:
+File: file_utils.py
 
 Copyright 2017 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
@@ -24,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import csv
 import os
-import re
 import pandas
+import glob
 import subprocess
 from nestpp.loggerpp import get_module_logger
 from nestpp.utils import input_with_timeout
@@ -65,100 +64,62 @@ def get_max_csv_cols(path):
     return maxcols
 
 
-def getTimedFileList(directory, prefix):
-    """Get list of files for each snapshot time."""
-    completefilelist = getFileList(directory, prefix)
-    timelist = []
+def combine_tsv_files_column_wise(directory, shell_glob):
+    """Combines tab separated files with constant number of fields
 
-    if not completefilelist:
-        return None
+    :directory: directory to work in
+    :shell_glob: shell_glob of files
+    :returns: combined dataframe or empty dataframe if files not found
 
-    for afile in completefilelist:
-        filename = afile[len(directory):]
-        time = (filename.split('-')[5])[0:-4]
-        timelist.append(time)
-
-    timelist = set(timelist)
-    filedict = {}
-
-    for afile in completefilelist:
-        for time in timelist:
-            if ("-" + time + ".txt") in afile:
-                if time in filedict:
-                    filedict[time].append(afile)
-                else:
-                    filedict[time] = [afile]
-
-    if len(filedict) > 0:
-        return filedict
-    return None
-
-
-def getFileList(directory, prefix):
-    """Get list of files with prefix."""
-    directorylist = os.listdir(directory)
-    prefixregex = re.compile(re.escape(prefix))
-    filelist = []
-
-    for entry in directorylist:
-        fullentry = os.path.join(directory + '/' + entry)
-        if os.path.isfile(fullentry):
-            if(prefixregex.match(entry)):
-                filelist.append(fullentry)
-
-    if len(filelist) > 0:
-        return filelist
-    return None
-
-
-def combineTimedTSVColDataFiles(directory, prefix):
-    """Combine TSV files columnwise."""
-    filedict = getTimedFileList(directory, prefix)
-    if not filedict:
+    """
+    file_list = glob.glob('{}/{}'.format(directory, shell_glob))
+    if not file_list:
         return {}
 
-    combineddataframelist = {}
+    combineddataframe = pandas.DataFrame()
+    dataframes = []
+    for entry in file_list:
+        lgr.info("Reading {}".format(entry))
+        dataframe = pandas.read_csv(entry, skiprows=1, sep='\t',
+                                    skipinitialspace=True,
+                                    skip_blank_lines=True, dtype=float,
+                                    warn_bad_lines=True,
+                                    lineterminator='\n', header=None,
+                                    index_col=0, error_bad_lines=False)
 
-    for time, filelist in filedict.items():
-        dataframes = []
-        for entry in filelist:
-            print("Reading {}".format(entry))
-            dataframe = pandas.read_csv(entry, skiprows=1, sep='\t',
-                                        skipinitialspace=True,
-                                        skip_blank_lines=True, dtype=float,
-                                        warn_bad_lines=True,
-                                        lineterminator='\n', header=None,
-                                        index_col=0, error_bad_lines=False)
+        dataframes.append(dataframe)
 
-            dataframes.append(dataframe)
-
-        print("Combined dataframe..")
+        lgr.info("Combined dataframe..")
         combineddataframe = pandas.concat(dataframes, axis=0)
-        combineddataframelist[time] = combineddataframe.sort_index()
 
-    return combineddataframelist
+    return combineddataframe
 
 
-def combineCSVRowLists(directory, prefix):
+def combine_var_csv_files_column_wise(directory, shell_glob):
+    """Combines comma separated files with variable numbers of fields in each row.
+
+    If the m input files are of all of the form:
+    time, n comma separated values
+
+    then, this returns:
+    time, (m x n) comma separated values
+
+    Note that each line may have a different number of columns, and lines with
+    same index in different files may also have different numbers of columns.
+
+    :directory: directory where files are
+    :shell_glob: shell_glob of files
+    :returns: combined dataframe or empty dataframe if files not found.
+
     """
-    Combine comma separated files row wise.
-
-    Each line may have a different number of fields.
-
-    Format:
-    time, comma separated values
-
-    Returns:
-    time, comma separated values concatenated from all files.
-    """
-    filelist = getFileList(directory, prefix)
-    if not filelist:
+    file_list = glob.glob('{}/{}'.format(directory, shell_glob))
+    if not file_list:
         return pandas.DataFrame()
 
     dataframes = []
 
-    for entry in filelist:
-        print("Reading {}".format(entry))
+    for entry in file_list:
+        lgr.info("Reading {}".format(entry))
         if os.stat(entry).st_size != 0:
             # Last line contains the maximum number of fields in any line,
             # so use this to size our df. Pandas can manage lines shorter
@@ -167,7 +128,7 @@ def combineCSVRowLists(directory, prefix):
             # Ignore ',\n'
             max_columns = int(float(
                 subprocess.check_output(['tail', '-1', entry])[0:-2])) + 1
-            print("Max cols is: {}".format(max_columns))
+            lgr.debug("Max cols is: {}".format(max_columns))
 
             dataframe = pandas.read_csv(entry, skiprows=1, sep=',',
                                         skipinitialspace=True,
@@ -183,34 +144,36 @@ def combineCSVRowLists(directory, prefix):
 
             dataframes.append(dataframe)
         else:
-            print("Skipping empty file, {}".format(entry))
+            lgr.warning("Skipping empty file, {}".format(entry))
 
-    print("Combining dataframes..")
+    lgr.info("Combining dataframes..")
     combineddataframe = pandas.concat(dataframes, axis=1)
 
     return combineddataframe
 
 
-def combineTSVRowData(directory, prefix):
-    """
-    Combine tab separated files row wise.
+def sum_columns_in_multiple_files(directory, shell_glob):
+    """Sums up the columns in different rank files.
 
-    The difference here is that the columns depict different things, unlike
-    the csv lists in the other method.
-
-    Format:
+    If the input files are each of the form:
     time    info1   info2   info3   info4..
 
-    Returns:
+    This will return a dataframe:
         time    sumoffiles(info1)   sumoffiles(info2)...
+
+
+    :directory: the directory to act in
+    :shell_glob: the shell_glob of the various files
+    :returns: summed up dataframe
+
     """
-    filelist = getFileList(directory, prefix)
-    if not filelist:
+    file_list = glob.glob('{}/{}'.format(directory, shell_glob))
+    if not file_list:
         return pandas.DataFrame()
 
     dataframes = []
 
-    for entry in filelist:
+    for entry in file_list:
         if os.stat(entry).st_size != 0:
             dataframe = pandas.read_csv(entry, skiprows=1, sep='\t',
                                         skipinitialspace=True,
@@ -221,7 +184,7 @@ def combineTSVRowData(directory, prefix):
 
             dataframes.append(dataframe)
         else:
-            print("Skipping empty file, {}".format(entry))
+            lgr.warning("Skipping empty file, {}".format(entry))
 
     summeddf = dataframes.pop(0)
     for dataframe in dataframes:
@@ -230,23 +193,19 @@ def combineTSVRowData(directory, prefix):
     return summeddf
 
 
-def reprocess_raw_files(prefixlist):
+def reprocess_raw_files(shell_globs):
     """Ask if files should be reprocessed if found."""
-    filelist = os.listdir()
-    filesfound = []
-    for entry in filelist:
-        for prefix in prefixlist:
-            if prefix in entry:
-                if ".png" not in entry:
-                    filesfound.append(entry)
+    files_found = []
+    for shell_glob in shell_globs:
+        files_found.append(glob.glob("./{}".format(shell_glob)))
 
-    if len(filesfound) == 0:
+    if len(files_found) == 0:
         return True
 
-    filesfound.sort()
-    if len(filesfound) > 0:
-        lgr.info("Generated files found: {}".format(len(filesfound)))
-        for entry in filesfound:
+    files_found.sort()
+    if len(files_found) > 0:
+        lgr.info("Generated files found: {}".format(len(files_found)))
+        for entry in files_found:
             lgr.info("- {}".format(entry))
 
         while True:
