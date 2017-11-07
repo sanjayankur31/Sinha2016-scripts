6ce834b14cb7c22aa2c9f350916aabeee9831f85
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-11-07 22:26:46 +0000

    Add montage creator for firing rates

---
+++ sundry/viewing/make-montage-firing-rates.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Generate a montage of images so that they can be all seen at once
+# This file includes graphs to do with structural plasticity
+
+PREFIX=""
+
+usage()
+{
+    echo "montage.sh -y <year prefix>"
+    echo "Note, prefix has a trailing dash in it so that it can be left empty if required."
+}
+
+main ()
+{
+    montage \
+        "$PREFIX"mean-firing-rates-all-E-zoomed.png \
+        "$PREFIX"mean-firing-rates-all-I-zoomed.png \
+        "$PREFIX"mean-firing-rates-lpz_c_I-E-zoomed.png \
+        "$PREFIX"mean-firing-rates-lpz_b_I-E-zoomed.png \
+        "$PREFIX"mean-firing-rates-p_lpz_I-E-zoomed.png \
+        "$PREFIX"mean-firing-rates-o_I-E-zoomed.png \
+        -tile 2x3 -geometry +2+2  "$PREFIX"firing-rate-montage.png
+}
+
+
+
+# check for options
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 0
+fi
+
+while getopts "y:h" OPTION
+do
+    case $OPTION in
+        y)
+            PREFIX=$OPTARG
+            main
+            ;;
+        h)
+            usage
+            exit 1
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
