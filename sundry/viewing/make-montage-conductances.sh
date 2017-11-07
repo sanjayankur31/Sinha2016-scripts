a48031fa9baf799d3a5a69c6b2796ba14503c15b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-11-07 22:28:42 +0000

    Add montage creator for conductance graphs

---
+++ sundry/viewing/make-montage-conductances.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Generate a montage of images so that they can be all seen at once
+# This file includes graphs to do with conductances
+
+PREFIX=""
+
+usage()
+{
+    echo "montage.sh -y <year prefix>"
+    echo "Note, prefix has a trailing dash in it so that it can be left empty if required."
+}
+
+main ()
+{
+    montage \
+        "$PREFIX"all-mean-conductances.png \
+        "$PREFIX"all-total-conductances.png \
+        -tile 2x1 -geometry +2+2  "$PREFIX"conductance-montage.png
+}
+
+
+
+# check for options
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 0
+fi
+
+while getopts "y:h" OPTION
+do
+    case $OPTION in
+        y)
+            PREFIX=$OPTARG
+            main
+            ;;
+        h)
+            usage
+            exit 1
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
