4ab9694877935900b311c89ec8f14d7e259a8983
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-03-16 15:02:51 +0000

    Do not include empty dirs in file list

--- postprocess/bash/nest/postprocess-nest.sh
+++ postprocess/bash/nest/postprocess-nest.sh
@@ -124,7 +124,7 @@ fetch ()
     TMPDIR=$(mktemp -d)
     echo "TMPDIR is: $TMPDIR"
     echo "Fetching file list"
-    rsync --out-format="%l %n" --no-v --dry-run --recursive "$CLUSTER_PATH/$DIRNAME"/result/ "$DIRNAME" > "$TMPDIR"/"$DIRNAME".all
+    rsync --out-format="%l %n" -m --no-v --dry-run --recursive "$CLUSTER_PATH/$DIRNAME"/result/ "$DIRNAME" > "$TMPDIR"/"$DIRNAME".all
 
     echo "Calculating chunks"
     cat $TMPDIR/"$DIRNAME".all | cut -d\  -f2 | split -d -a1 -n r/10 - $TMPDIR/chunk.
