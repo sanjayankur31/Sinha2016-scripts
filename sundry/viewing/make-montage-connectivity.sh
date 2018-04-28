c3c62ed964b0de286af7d187678848c105140f0b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-04-28 07:36:42 +0100

    New connectivity montages

---
+++ sundry/viewing/make-montage-connectivity.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+# Generate a montage of images so that they can be all seen at once
+# This file includes graphs of initial connectivity
+
+PREFIX=""
+
+usage()
+{
+    echo "make-montage-initial-connectivity.sh -y <year prefix>"
+    echo "Note, prefix has a trailing dash in it so that it can be left empty if required."
+}
+
+main ()
+{
+    # Get the number of columns we have. One column for each time.
+    COLS=$(ls -- *75-connections-hist-EE*incoming.png | wc -l)
+
+    montage \
+        "$PREFIX"08-syn_conns-E-to-lpz_c_E.png \
+        "$PREFIX"08-syn_conns-E-to-lpz_b_E.png \
+        "$PREFIX"08-syn_conns-E-to-p_lpz_E.png \
+        "$PREFIX"08-syn_conns-E-to-o_E.png \
+        "$PREFIX"08-syn_conns-I-to-lpz_c_E.png \
+        "$PREFIX"08-syn_conns-I-to-lpz_b_E.png \
+        "$PREFIX"08-syn_conns-I-to-p_lpz_E.png \
+        "$PREFIX"08-syn_conns-I-to-o_E.png \
+        "$PREFIX"081-connection-clustered-histograms-lpz_c_E.png \
+        "$PREFIX"081-connection-clustered-histograms-lpz_b_E.png \
+        "$PREFIX"081-connection-clustered-histograms-p_lpz_E.png \
+        "$PREFIX"081-connection-clustered-histograms-o_E.png \
+        "$PREFIX"081-connection-rowstacked-histograms-lpz_c_E.png \
+        "$PREFIX"081-connection-rowstacked-histograms-lpz_b_E.png \
+        "$PREFIX"081-connection-rowstacked-histograms-p_lpz_E.png \
+        "$PREFIX"081-connection-rowstacked-histograms-o_E.png \
+        -tile 4x4 -geometry +2+2  "$PREFIX"75-connections-E-montage.png
+
+    montage \
+        "$PREFIX"08-syn_conns-E-to-lpz_c_I.png \
+        "$PREFIX"08-syn_conns-E-to-lpz_b_I.png \
+        "$PREFIX"08-syn_conns-E-to-p_lpz_I.png \
+        "$PREFIX"08-syn_conns-E-to-o_I.png \
+        "$PREFIX"08-syn_conns-I-to-lpz_c_I.png \
+        "$PREFIX"08-syn_conns-I-to-lpz_b_I.png \
+        "$PREFIX"08-syn_conns-I-to-p_lpz_I.png \
+        "$PREFIX"08-syn_conns-I-to-o_I.png \
+        "$PREFIX"081-connection-clustered-histograms-lpz_c_I.png \
+        "$PREFIX"081-connection-clustered-histograms-lpz_b_I.png \
+        "$PREFIX"081-connection-clustered-histograms-p_lpz_I.png \
+        "$PREFIX"081-connection-clustered-histograms-o_I.png \
+        "$PREFIX"081-connection-rowstacked-histograms-lpz_c_I.png \
+        "$PREFIX"081-connection-rowstacked-histograms-lpz_b_I.png \
+        "$PREFIX"081-connection-rowstacked-histograms-p_lpz_I.png \
+        "$PREFIX"081-connection-rowstacked-histograms-o_I.png \
+        -tile 4x4 -geometry +2+2  "$PREFIX"75-connections-I-montage.png
+
+    montage \
+        "$PREFIX"75-connections-top-EE-*-incoming.png \
+        "$PREFIX"75-connections-hist-EE-*-incoming.png \
+        "$PREFIX"75-connections-top-IE-*-incoming.png \
+        "$PREFIX"75-connections-hist-IE-*-incoming.png \
+        "$PREFIX"75-connections-top-EI-*-incoming.png \
+        "$PREFIX"75-connections-hist-EI-*-incoming.png \
+        "$PREFIX"75-connections-top-II-*-incoming.png \
+        "$PREFIX"75-connections-hist-II-*-incoming.png \
+        -tile "$COLS"x8 -geometry +2+2  "$PREFIX"75-connections-incoming-time-lapse-montage.png
+
+    montage \
+        "$PREFIX"75-connections-top-EE-*-outgoing.png \
+        "$PREFIX"75-connections-hist-EE-*-outgoing.png \
+        "$PREFIX"75-connections-top-IE-*-outgoing.png \
+        "$PREFIX"75-connections-hist-IE-*-outgoing.png \
+        "$PREFIX"75-connections-top-EI-*-outgoing.png \
+        "$PREFIX"75-connections-hist-EI-*-outgoing.png \
+        "$PREFIX"75-connections-top-II-*-outgoing.png \
+        "$PREFIX"75-connections-hist-II-*-outgoing.png \
+        -tile "$COLS"x8 -geometry +2+2  "$PREFIX"75-connections-outgoing-time-lapse-montage.png
+}
+
+
+
+# check for options
+if [ "$#" -eq 0 ]; then
+    usage
+    exit 0
+fi
+
+while getopts "y:h" OPTION
+do
+    case $OPTION in
+        y)
+            PREFIX=$OPTARG
+            main
+            ;;
+        h)
+            usage
+            exit 1
+            ;;
+        ?)
+            usage
+            exit 1
+            ;;
+    esac
+done
