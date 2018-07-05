b4ee1e8a98077bd981f81d8ac93e87e96f2b1b25
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-07-05 13:43:14 +0100

    Remove wrong license info included by tempate

--- postprocess/py/nestpp/file_utils.py
+++ postprocess/py/nestpp/file_utils.py
@@ -3,26 +3,12 @@ Utilities dealing with files.
 
 File: file_utils.py
 
-
 Note that we always use the 0th column as the index column, and we never
 specify a header row while reading dataframes. This is a convention that must
 be followed while using these functions.
 
-Copyright 2017 Ankur Sinha
+Copyright 2018 Ankur Sinha
 Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 
