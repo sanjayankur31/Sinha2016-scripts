7c6cac6fd6cb19a9eee2a5f9041602f1a62e3246
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2018-08-10 15:08:51 +0100

    Also create multi sim conductance graphs

--- sundry/viewing/make-multi-sim-montage.py
+++ sundry/viewing/make-multi-sim-montage.py
@@ -45,6 +45,7 @@ def montagise(simlist):
                                                     cleaned_list))
         connections(cleaned_list, timestamp)
         neuron_metrics(cleaned_list, timestamp)
+        conductances(cleaned_list, timestamp)
 
 
 def neuron_metrics(simlist, timestamp):
@@ -166,6 +167,101 @@ def connections(simlist, timestamp):
     for region in regions:
         output_file = (
             "{}-multi-sim-connections-{}-montage.png".format(
+                timestamp, region
+            )
+        )
+        print("Generating {}...".format(output_file))
+        args = []
+        # Add growth curves on top
+        gcs = ['growth-curves-E', 'growth-curves-I']
+        for gc in gcs:
+            for sim in simlist:
+                fname = (
+                    os.path.join(sim, "{}-{}.png".format(
+                        sim, gc
+                    ))
+                )
+                if os.path.isfile(fname):
+                    args.append(fname)
+                else:
+                    print(
+                        "{}: file not found. Exiting.".format(
+                            fname
+                        ),
+                        file=sys.stderr
+                    )
+                    sys.exit(-2)
+
+        for graph in graphs:
+            for sim in simlist:
+                fname = (
+                    os.path.join(sim, "{}-{}-{}.png".format(
+                        sim, graph, region
+                    ))
+                )
+                if os.path.isfile(fname):
+                    args.append(fname)
+                else:
+                    print(
+                        "{}: file not found. Exiting.".format(
+                            fname
+                        ),
+                        file=sys.stderr
+                    )
+                    sys.exit(-2)
+
+        # Final bits
+        args += [
+            "-title", "'{}'".format(
+                ", ".join(str(sim) for sim in simlist)
+            ),
+            "-tile", "{}x{}".format(num_cols, len(graphs) + len(gcs)),
+            "-pointsize", "28",
+            "-font", "OpenSans",
+            "-geometry", "+2+2",
+            output_file
+        ]
+
+        try:
+            print("Running montage with args: {}".format(args))
+            status = subprocess.run(args=['montage'] + args,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+            status.check_returncode()
+        except CalledProcessError as cpe:
+            print(
+                "{} errored with return code {}".format(
+                    cpe.cmd, cpe.returncode))
+            print("\n" + cpe.stderr.decode())
+        else:
+            print("{} Montage created".format(output_file))
+
+
+def conductances(simlist, timestamp):
+    """Make the conductances montage
+
+    :simlist: list of simulations
+    :timestamp: time at which montages are generated
+    :returns: nothing
+
+    """
+    print("Generating conductances montage")
+    # Each simulation gets a column
+    num_cols = len(simlist)
+    graphs = [
+        "02-calcium",
+        "08-total-conductances-E-to",
+        "08-total-conductances-I-to",
+        "08-mean-conductances-E-to",
+        "08-mean-conductances-I-to",
+    ]
+
+    regions = ["lpz_c_E", "lpz_b_E", "p_lpz_E", "o_E"]
+    regions += ["lpz_c_I", "lpz_b_I", "p_lpz_I", "o_I"]
+
+    for region in regions:
+        output_file = (
+            "{}-multi-sim-conductances-{}-montage.png".format(
                 timestamp, region
             )
         )
